4b9a03952a8b170ee2635d1d406dbbef5d7614b2
eliclaggett <elijah.claggett@gmail.com>
2024-03-25 09:34:22 -0400

    Update packages

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,26 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig, searchForWorkspaceRoot } from "vite";
 import restart from "vite-plugin-restart";
 import UnoCSS from "unocss/vite";
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+import findConfig from "find-config";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+let dotEnvPath = null;
+if (fs.existsSync('/home/ubuntu/eli')) {
+  dotEnvPath = findConfig('.env', {cwd: '/home/ubuntu/eli/experiment'});
+} else if (fs.existsSync('/Users/eclagget/Code/experiment')) {
+  dotEnvPath = findConfig('.env', {cwd: '/Users/eclagget/Code/experiment/chat-cooperation'});
+}
+
+if (dotEnvPath) {
+  const envFile = dotenv.parse(fs.readFileSync(dotEnvPath));
+  for (const key of Object.keys(envFile)) {
+    process.env[key] = envFile[key];
+  }
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +28,7 @@ export default defineConfig({
     exclude: ["@empirica/tajriba", "@empirica/core"],
   },
   server: {
-    port: 8844,
+    port: process.env['PORT_EMPIRICA_PROXY'],
     open: false,
     strictPort: true,
     host: "0.0.0.0",
@@ -20,9 +38,14 @@ export default defineConfig({
         searchForWorkspaceRoot(process.cwd()),
       ],
     },
+    hmr: {
+      host: "localhost",
+      protocol: "ws",
+      clientPort: process.env['PORT_EMPIRICA_PROXY']
+    }
   },
   build: {
-    minify: false,
+    minify: false
   },
   clearScreen: false,
   plugins: [
@@ -34,11 +57,12 @@ export default defineConfig({
       ],
     }),
     UnoCSS(),
-    reactRefresh(),
+    react(),
+    nodePolyfills(),
   ],
   define: {
     "process.env": {
       NODE_ENV: process.env.NODE_ENV || "development",
-    },
-  },
+    }
+  }
 });
