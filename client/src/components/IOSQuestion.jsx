5a384d0f2e8e431ff407f404ec4be81e5194f71f
eliclaggett <elijah.claggett@gmail.com>
2024-08-12 19:29:17 +0900

    Fix no image issue

--- client/src/components/IOSQuestion.jsx
+++ client/src/components/IOSQuestion.jsx
@@ -9,21 +9,21 @@ import React from "react";
 import { FormControl, FormLabel, Typography, RadioGroup } from '@mui/joy';
 import ImageRadioButton from "./ImageRadioButton.jsx";
 
-import IOS1Partner from '../images/IOS 1 Partner.svg';
-import IOS2Partner from '../images/IOS 2 Partner.svg';
-import IOS3Partner from '../images/IOS 3 Partner.svg';
-import IOS4Partner from '../images/IOS 4 Partner.svg';
-import IOS5Partner from '../images/IOS 5 Partner.svg';
-import IOS6Partner from '../images/IOS 6 Partner.svg';
-import IOS7Partner from '../images/IOS 7 Partner.svg';
+// import IOS1Partner from '../images/IOS 1 Partner.svg';
+// import IOS2Partner from '../images/IOS 2 Partner.svg';
+// import IOS3Partner from '../images/IOS 3 Partner.svg';
+// import IOS4Partner from '../images/IOS 4 Partner.svg';
+// import IOS5Partner from '../images/IOS 5 Partner.svg';
+// import IOS6Partner from '../images/IOS 6 Partner.svg';
+// import IOS7Partner from '../images/IOS 7 Partner.svg';
 
-import IOS1AI from '../images/IOS 1 AI.svg';
-import IOS2AI from '../images/IOS 2 AI.svg';
-import IOS3AI from '../images/IOS 3 AI.svg';
-import IOS4AI from '../images/IOS 4 AI.svg';
-import IOS5AI from '../images/IOS 5 AI.svg';
-import IOS6AI from '../images/IOS 6 AI.svg';
-import IOS7AI from '../images/IOS 7 AI.svg';
+// import IOS1AI from '../images/IOS 1 AI.svg';
+// import IOS2AI from '../images/IOS 2 AI.svg';
+// import IOS3AI from '../images/IOS 3 AI.svg';
+// import IOS4AI from '../images/IOS 4 AI.svg';
+// import IOS5AI from '../images/IOS 5 AI.svg';
+// import IOS6AI from '../images/IOS 6 AI.svg';
+// import IOS7AI from '../images/IOS 7 AI.svg';
 
 
 
@@ -47,13 +47,13 @@ export default function IOSQuestion({
                     sx={{ display: 'flex', flexDirection: 'row', mx: 'auto', flexWrap: 'wrap', gap: 2, justifyContent: 'center' }}
                     onChange={onChange}
                 >
-                    <ImageRadioButton src={"images/IOS 1"+typeTxt+".svg"} value="0" />
-                    <ImageRadioButton src={"images/IOS 2"+typeTxt+".svg"} value="1" />
-                    <ImageRadioButton src={"images/IOS 3"+typeTxt+".svg"} value="2" />
-                    <ImageRadioButton src={"images/IOS 4"+typeTxt+".svg"} value="3" />
-                    <ImageRadioButton src={"images/IOS 5"+typeTxt+".svg"} value="4" />
-                    <ImageRadioButton src={"images/IOS 6"+typeTxt+".svg"} value="5" />
-                    <ImageRadioButton src={"images/IOS 7"+typeTxt+".svg"} value="6" />
+                    <ImageRadioButton src={"/images/IOS 1"+typeTxt+".svg"} value="0" />
+                    <ImageRadioButton src={"/images/IOS 2"+typeTxt+".svg"} value="1" />
+                    <ImageRadioButton src={"/images/IOS 3"+typeTxt+".svg"} value="2" />
+                    <ImageRadioButton src={"/images/IOS 4"+typeTxt+".svg"} value="3" />
+                    <ImageRadioButton src={"/images/IOS 5"+typeTxt+".svg"} value="4" />
+                    <ImageRadioButton src={"/images/IOS 6"+typeTxt+".svg"} value="5" />
+                    <ImageRadioButton src={"/images/IOS 7"+typeTxt+".svg"} value="6" />
                 </RadioGroup>
             </FormControl>
     );
